8617ed0b0b0e5b26d815ec27cd881ad27d0931b4
Daniel <58776214+JDany94@users.noreply.github.com>
2022-06-02 19:09:22 +0100

    deploy

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,19 +1,27 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Recuperar contraseña
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+      <form 
+        className="my-10 bg-white shadow rounded-lg p-10"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <input
             id="email"
             type="email"
             placeholder="Email"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <input
